e89a0f6bd0ec2c65a3bf60ea3ae1973b5b7254de
Mechanel <nyutanislavsky@qq.com>
2023-11-17 00:21:29 +0800

    new 应玚，柳隐，张布，张葳

--- lunar_sp1.lua
+++ lunar_sp1.lua
@@ -1,6 +1,8 @@
 local extension = Package("lunar_sp1")
 extension.extensionName = "lunar"
 
+local U = require "packages/utility/utility"
+
 Fk:loadTranslationTable{
   lunar_sp1 = "新月杀专属",
   fk = "新月",
@@ -273,7 +275,7 @@ local fk__yicha = fk.CreateTriggerSkill{
 
   refresh_events = {fk.AfterCardsMove},
   can_refresh = function(self, event, target, player, data)
-    if player:hasSkill(self.name, true) then
+    if player:hasSkill(self, true) then
       local room = player.room
       for _, move in ipairs(data) do
         if move.toArea == Card.DiscardPile then
@@ -315,12 +317,17 @@ Fk:loadTranslationTable{
   ['@fk__yicha-turn'] = '益察',
   ["#fk__yicha-discard"] = "益察：观看牌堆顶 %arg 张牌，可将其中任意张牌置入弃牌堆",
 }
+
+-- 新月杀第三届DIY选拔： 应玚，柳隐，张布，张葳
+-- 主催：理塘王
+
 local yingyang = General(extension, "fk__yingyang", "wei", 3)
 local fk__guici = fk.CreateTriggerSkill{
   name = "fk__guici",
   events = {fk.RoundStart},
   anim_type = "switch",
   switch_skill_name = "fk__guici",
+  frequency = Skill.Compulsory,
   can_trigger = function(self, event, target, player, data)
     return player:hasSkill(self)
   end,
@@ -328,23 +335,27 @@ local fk__guici = fk.CreateTriggerSkill{
     local room = player.room
     local patternTable = {}
     if player:getSwitchSkillState(self.name, true) == fk.SwitchYang then
+      patternTable = { ["heart"] = {}, ["diamond"] = {}, ["spade"] = {}, ["club"] = {} }
       for _, id in ipairs(room.draw_pile) do
-        local suit = Fk:getCardById(id):getSuitString()
-        if suit ~= "nosuit" then
-          patternTable[suit] = patternTable[suit] or {}
-          table.insert(patternTable[suit], id)
+        local pattern = Fk:getCardById(id):getSuitString()
+        if patternTable[pattern] then
+          table.insert(patternTable[pattern], id)
         end
       end
     else
+      patternTable = { ["basic"] = {}, ["trick"] = {}, ["equip"] = {} }
       for _, id in ipairs(room.draw_pile) do
-        local ty = Fk:getCardById(id):getTypeString()
-        patternTable[ty] = patternTable[ty] or {}
-        table.insert(patternTable[ty], id)
+        local pattern = Fk:getCardById(id):getTypeString()
+        if patternTable[pattern] then
+          table.insert(patternTable[pattern], id)
+        end
       end
     end
     local get = {}
     for _, ids in pairs(patternTable) do
-      table.insert(get, table.random(ids))
+      if #ids > 0 then
+        table.insert(get, table.random(ids))
+      end
     end
     if #get > 0 then
       local dummy = Fk:cloneCard("dilu")
@@ -390,12 +401,20 @@ local fk__beili = fk.CreateTriggerSkill{
 yingyang:addSkill(fk__beili)
 Fk:loadTranslationTable{
   ["fk__yingyang"] = "应玚",
+  ["designer:fk__yingyang"] = "小嘤嘤",
   ["fk__guici"] = "瑰词",
   [":fk__guici"] = "转换技，锁定技，每轮开始时，你从牌堆中获得：阳：四张花色各不相同的牌；阴：三张类型各不相同的牌。",
   ["@@fk__guici-inhand"] = "瑰词",
   ["fk__beili"] = "悲离",
-  [":fk__beili"] = "每当一名角色受到伤害后，你可以弃置一张牌，令其摸一张牌，若你以此法弃置了〖瑰词〗牌，你摸一张牌。",
-  ["#fk__beili-discard"] = "悲离：你可弃一张牌，令 %dest 摸一张牌，若弃置〖瑰词〗牌，你摸一张牌。",
+  [":fk__beili"] = "每当一名角色受到伤害后，你可以弃置一张牌，令其摸一张牌，若你以此法弃置了“瑰词”牌，你摸一张牌。",
+  ["#fk__beili-discard"] = "悲离：你可弃一张牌，令 %dest 摸一张牌，若弃置“瑰词”牌，你摸一张牌。",
+
+  -- CV：万事屋
+  ["$fk__guici1"] = "涉津洛之阪泉兮，播九道乎中州。",
+  ["$fk__guici2"] = "衔积石之重险兮，披山麓而溢浮。",
+  ["$fk__beili1"] = "有鸟孤栖，哀鸣北林。嗟我怀矣，感物伤心。",
+  ["$fk__beili2"] = "朝雁鸣云中，音响一何哀。",
+  ["~fk__yingyang"] = "胸中千言未述…所志皆不遂…",
 }
 local liuyin = General(extension, "fk__liuyin", "shu", 4)
 local fk__gushou = fk.CreateTriggerSkill{
@@ -445,15 +464,15 @@ local fk__gushou = fk.CreateTriggerSkill{
   end,
 }
 liuyin:addSkill(fk__gushou)
-local fk__pifa = fk.CreateTriggerSkill{
-  name = "fk__pifa",
+local fk__fapi = fk.CreateTriggerSkill{
+  name = "fk__fapi",
   events = {fk.EventPhaseStart},
   anim_type = "offensive",
   can_trigger = function(self, event, target, player, data)
     return player:hasSkill(self) and player ~= target and target.phase == Player.Finish and player.hp > 0
   end,
   on_cost = function(self, event, target, player, data)
-    return player.room:askForSkillInvoke(player, self.name, nil, "#fk__pifa-invoke::"..target.id)
+    return player.room:askForSkillInvoke(player, self.name, nil, "#fk__fapi-invoke::"..target.id)
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
@@ -472,18 +491,25 @@ local fk__pifa = fk.CreateTriggerSkill{
     end
   end,
 }
-liuyin:addSkill(fk__pifa)
+liuyin:addSkill(fk__fapi)
 Fk:loadTranslationTable{
   ["fk__liuyin"] = "柳隐",
+  ["designer:fk__liuyin"] = "白幽",
   ["fk__gushou"] = "固守",
   [":fk__gushou"] = "每回合每项各限一次，当你受到伤害时，你可以：1.弃置一张牌，然后回复1点体力；2.失去一点体力，防止此伤害，然后摸一张牌。",
   ["fk__gushou_discard"] = "弃置一张牌，然后回复1点体力",
   ["fk__gushou_losehp"] = "失去一点体力，防止此伤害，摸一张牌",
   ["#fk__gushou-losehp"] = "固守：失去一点体力，防止此伤害，然后摸一张牌",
   ["#fk__gushou-discard"] = "固守：弃置一张牌，回复1点体力",
-  ["fk__pifa"] = "伐疲",
-  [":fk__pifa"] = "其他角色的结束阶段，你可以失去1点体力，摸一张牌，视为对其使用一张无距离限制的【杀】，若此【杀】造成伤害，你摸一张牌。",
-  ["#fk__pifa-invoke"] = "伐疲：失去1点体力，摸一张牌，视为对 %dest 使用一张【杀】",
+  ["fk__fapi"] = "伐疲",
+  [":fk__fapi"] = "其他角色的结束阶段，你可以失去1点体力，摸一张牌，视为对其使用一张无距离限制的【杀】，若此【杀】造成伤害，你摸一张牌。",
+  ["#fk__fapi-invoke"] = "伐疲：失去1点体力，摸一张牌，视为对 %dest 使用一张【杀】",
+
+  ["$fk__gushou1"] = "坚守此城，援军不日必至。",
+  ["$fk__gushou2"] = "诸将无虑，大将军早有筹谋。",
+  ["$fk__fapi1"] = "守城之善者，出城破敌也！",
+  ["$fk__fapi2"] = "久战之师，强弩之末，不能穿鲁缟。",
+  ["~fk__liuyin"] = "汉臣守土有责，无奈汉主先降！",
 }
 local zhangbu = General(extension, "fk__zhangbu", "wu", 3)
 local fk__guzhu = fk.CreateTriggerSkill{
@@ -518,7 +544,7 @@ local fk__zhuanzheng = fk.CreateTriggerSkill{
   anim_type = "support",
   events = {fk.AfterCardsMove},
   can_trigger = function(self, event, target, player, data)
-    if not player:hasSkill(self.name) or player.hp < 1 then return end
+    if not player:hasSkill(self) or player.hp < 1 then return end
     for _, move in ipairs(data) do
       if move.from and player.room:getPlayerById(move.from):isKongcheng() then
         for _, info in ipairs(move.moveInfo) do
@@ -566,14 +592,90 @@ local fk__zhuanzheng = fk.CreateTriggerSkill{
 zhangbu:addSkill(fk__zhuanzheng)
 Fk:loadTranslationTable{
   ["fk__zhangbu"] = "张布",
+  ["designer:fk__zhangbu"] = "理塘王",
   ["fk__guzhu"] = "孤注",
-  [":fk__guzhu"] = "一名角色使用基本牌指定目标后，你可以弃置所有手牌，令此牌额外结算一次",
+  [":fk__guzhu"] = "一名角色使用基本牌指定目标后，你可以弃置所有手牌，令此牌额外结算一次。",
   ["#fk__guzhu-invoke"] = "孤注：你可以弃置所有手牌，令 %dest 使用的 %arg 额外结算一次",
   ["fk__zhuanzheng"] = "专政",
   [":fk__zhuanzheng"] = "一名角色失去手牌后，若其没有手牌，你可以失去1点体力，令其将手牌摸至体力上限（至多摸五张）。",
   ["#fk__zhuanzheng-invoke"] = "专政：你可以失去1点体力，令 %dest 将手牌摸至体力上限（至多摸五张）",
+
+  ["$fk__guzhu1"] = "今进退无路，何不效伊尹霍光？",
+  ["$fk__guzhu2"] = "孙綝，虎狼也，岂可安坐待死？",
+  ["$fk__zhuanzheng1"] = "国朝之事，皆系我一身。",
+  ["$fk__zhuanzheng2"] = "布政之责，使内外无患。",
+  ["$fk__zhuanzheng3"] = "放肆！",
+  ["~fk__zhangbu"] = "起浮半生如怒涛…终随江水去…",
 }
 
+local zhangwei = General(extension, "fk__zhangwei", "qun", 4, 4, General.Female)
+local fk__xiaorong = fk.CreateTriggerSkill{
+  name = "fk__xiaorong",
+  anim_type = "offensive",
+  events = {fk.AfterCardTargetDeclared},
+  can_trigger = function (self, event, target, player, data)
+    return target == player and player:hasSkill(self) and data.card.trueName == "slash" and #TargetGroup:getRealTargets(data.tos) == 1 and table.find(player.room.alive_players, function (p)
+      return p:distanceTo(player) == 1
+    end)
+  end,
+  on_cost = function (self, event, target, player, data)
+    local room = player.room
+    local targets = U.getUseExtraTargets(room, data)
+    if #targets > 0 then
+      local num = #table.filter(player.room.alive_players, function (p)
+        return p:distanceTo(player) == 1
+      end)
+      local tos = room:askForChoosePlayers(player, targets, num, num, "#fk__xiaorong:::" .. num, self.name, true)
+      if #tos > 0 then
+        self.cost_data = tos
+        return true
+      end
+    end
+  end,
+  on_use = function (self, event, target, player, data)
+    table.forEach(self.cost_data, function (id)
+      table.insert(data.tos, {id})
+    end)
+    if #TargetGroup:getRealTargets(data.tos) > player.hp then
+      local card = Fk:cloneCard("duel")
+      card.skillName = self.name
+      card:addSubcard(data.card)
+      data.card = card
+    end
+  end,
+}
+zhangwei:addSkill(fk__xiaorong)
+local fk__yiyong = fk.CreateTriggerSkill{
+  name = "fk__yiyong",
+  anim_type = "drawcard",
+  events = {fk.Damage},
+  can_trigger = function (self, event, target, player, data)
+    return target == player and player:hasSkill(self) and data.to:getHandcardNum() > player:getHandcardNum()
+  end,
+  on_use = function (self, event, target, player, data)
+    player:drawCards(2, self.name)
+    player.room:addPlayerMark(player, "@fk__yiyong-turn")
+  end
+}
+local fk__yiyong_buff = fk.CreateTargetModSkill{
+  name = "#fk__yiyong_buff",
+  residue_func = function(self, player, skill, scope)
+    if player:getMark("@fk__yiyong-turn") ~= 0 and skill.trueName == "slash_skill" and scope == Player.HistoryPhase then
+      return player:getMark("@fk__yiyong-turn")
+    end
+  end,
+}
+fk__yiyong:addRelatedSkill(fk__yiyong_buff)
+zhangwei:addSkill(fk__yiyong)
+Fk:loadTranslationTable{
+  ["fk__zhangwei"] = "张葳",
+  ["designer:fk__zhangwei"] = "郭攸之的设计修改者",
+  ["fk__xiaorong"] = "骁戎",
+  [":fk__xiaorong"] = "当你使用【杀】选择目标后，若目标角色数为1，你可以令X名其他角色也成为此【杀】的目标，然后若此【杀】的目标数大于你的体力值，你令此【杀】改为【决斗】（X为至你距离为1的角色）。",
+  ["fk__yiyong"] = "义勇",
+  [":fk__yiyong"] = "当你对手牌数大于你的角色造成伤害后，你可以摸两张牌，然后你本回合使用【杀】次数上限+1。",
 
-
+  ["#fk__xiaorong"] = "骁戎：你可令%arg名其他角色成为此【杀】的目标",
+  ["@fk__yiyong-turn"] = "义勇",
+}
 return extension
